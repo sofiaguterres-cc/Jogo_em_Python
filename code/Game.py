6257da959aeb9238d071a05a3404223beb3204a8
Sofia <sofiasouzaguterres@gmail.com>
2026-04-25 18:50:37 -0300

    Importação do game no main

--- code/Game.py
+++ code/Game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pygame
+
 class Game:
     def __init__(self):
         self.window = None
 
     def run(self, ):
-        pass
+        print('Código Iniciado')
+        pygame.init()
+        screen = pygame.display.set_mode(size = (600, 480))
+        print('Código Encerrado')
+
+        print('Looping Iniciado')
+        while True:
+            # Checagem de todos os eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() # Fechar janela
+                    quit() # Finalizar o Pygame
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import pygame
 
-print('Código Iniciado')
-pygame.init()
-screen = pygame.display.set_mode(size = (800, 600))
-print('Código Encerrado')
+from code.Game import Game
 
-print('Looping Iniciado')
-while True:
-    # Checagem de todos os eventos
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Fechar janela
-            quit() # Finalizar o Pygame
+game = Game()
+game.run()
